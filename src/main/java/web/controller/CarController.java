65bfd0b7176b918d6de9e479af27fc16fec58f4e
Samummm <mogila@tut.by>
2023-07-02 13:18:44 +0300

    ver 3.0

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,30 +6,25 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-
 @Controller
 public class CarController {
-
-	private static List<Car> carsList;
+	private static CarService carService;
 	@Autowired
-	public CarController(List<Car> cars) {
-		carsList = cars;
+	public CarController(CarService carService) {
+		this.carService = carService;
 	}
+//	private static CarServiceImpl carService;
 
 	@GetMapping(value = "/cars")
-	public static String printCars(@RequestParam(value = "count", required = false) Integer param, ModelMap model) {
-		Optional <Integer> value = Optional.ofNullable(param);
-
-		if (value.isPresent()) {
-				model.addAttribute("carsList", carsList.subList(0, value.get()));
-		} else {
-			model.addAttribute("carsList", carsList);
-		}
+	public static String printCars(@RequestParam(value = "count", required = false) Optional <Integer> param, ModelMap model) {
+				model.addAttribute("carsList", carService.getCars().subList(0, param.orElseGet(() -> carService.getCars().size())));
 		return "cars";
 	}
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,15 +1,23 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+@Service
 public class CarServiceImpl implements CarService {
     private List<Car> carsList;
 
     public CarServiceImpl() {
-    }
 
+    }
+    @Autowired
+    public CarServiceImpl(List<Car> cars) {
+        carsList = cars;
+    }
     public void setCars(List<Car> cars) {
         this.carsList = cars;
     }
